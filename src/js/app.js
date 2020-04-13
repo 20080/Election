1f0d1ea19cd759094f00c7d49ce93b341bf42aaa
20080 <surajkumar2091@gmail.com>
2020-04-13 11:25:05 +0530

    Cast votes

--- src/js/app.js
+++ src/js/app.js
@@ -11,7 +11,7 @@ App = {
   
     initWeb3: function() {
       if (typeof web3 !== 'undefined') {
-        // If a web3 instance is already provided by Meta Mask.
+        // If a web3 instance is already provided by Meta Mask.//
         App.web3Provider = web3.currentProvider;
         web3 = new Web3(web3.currentProvider);
       } else {
